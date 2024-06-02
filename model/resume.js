da42801f997d7b81328e5d9264d71738c0e3c82b
Developers Today <mshari7185@gmail.com>
2024-06-03 00:47:32 +0530

    added create, findByUserid, update and findbyskills

--- model/resume.js
+++ model/resume.js
@@ -3,12 +3,22 @@ const db = require('../config/db');
 
 const Resume = {
     create: (resumeData, callback) => {
-        const query = 'INSERT INTO resumes (user_id, skills, experience, education) VALUES (?, ?, ?, ?)';
-        db.query(query, [resumeData.user_id, resumeData.skills, resumeData.experience, resumeData.education], callback);
+        const query = 'INSERT INTO resumes (userId, skills, experience, education) VALUES (?, ?, ?, ?)';
+        db.query(query, [resumeData.userId, resumeData.skills, resumeData.experience, resumeData.education], callback);
+    },
+    findByUserId: (userId, callback) => {
+        const query = 'SELECT * FROM resumes WHERE userId = ?';
+        db.query(query, [userId], callback);
+    },
+    update: (resumeData, callback) => {
+        const query = 'UPDATE resumes SET skills = ?, experience = ?, education = ? WHERE userId = ?';
+        db.query(query, [resumeData.skills, resumeData.experience, resumeData.education, resumeData.userId], callback);
     },
     findBySkills: (skills, callback) => {
-        const query = 'SELECT * FROM resumes WHERE skills LIKE ?';
-        db.query(query, [`%${skills}%`], callback);
+        const skillsArray = skills.split(',');
+        const query = 'SELECT * FROM resumes WHERE ' + skillsArray.map(skill => 'skills LIKE ?').join(' OR ');
+        const queryParams = skillsArray.map(skill => `%${skill.trim()}%`);
+        db.query(query, queryParams, callback);
     }
 };
 
