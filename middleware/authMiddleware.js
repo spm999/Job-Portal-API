7cec3feeec34c1a80cacf2af098f5e08967c64ee
Developers Today <mshari7185@gmail.com>
2024-06-03 00:48:45 +0530

    verify token for normal user

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,18 +2,32 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-    const token = req.headers['x-access-token'];
-    if (!token) {
-        return res.status(403).send('No token provided.');
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({ error: 'Authorization header missing' });
     }
+
+    const token = authHeader.split(' ')[1];
 
     jwt.verify(token, 'secret', (err, decoded) => {
         if (err) {
-            return res.status(500).send('Failed to authenticate token.');
+          console.error('Token verification error:', err);
+          return res.status(403).json({ message: 'Forbidden: Invalid token' });
         }
+    
+        // Check if the decoded token contains the expected role ('user' in this case)
+        if (!decoded.role || decoded.role !== 'user') {
+          return res.status(403).json({ message: 'Forbidden: Not authorized as User' });
+        }
+    
+        // If the token is valid and the user has the expected role, add decoded user information to the request object
+        req.user = decoded;
         req.userId = decoded.id;
-        next();
-    });
+        req.userRole = decoded.role; // Extract the role from the decoded JWT payload
+
+        next(); // Continue to the next middleware or route handler
+      });
 };
 
 module.exports = verifyToken;
