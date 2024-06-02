983ec9a8b3ec533ce95f19bdd0a7b915a01869e7
Developers Today <mshari7185@gmail.com>
2024-06-03 00:53:42 +0530

    called everything in index

--- index.js
+++ index.js
@@ -2,7 +2,14 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const adminRoutes=require('./routes/adminRoutes')
 
+app.use(bodyParser.json());
+
+app.use('/user', authRoutes);
+app.use('/admin', adminRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
