69e66491ff235298fa6649e8ce6df346bbb179d9
Developers Today <mshari7185@gmail.com>
2024-06-03 00:46:44 +0530

    redefined model , added job_code, stringify the requirements, added error log, and added findOne to add only unique jobs

--- model/job.js
+++ model/job.js
@@ -3,8 +3,33 @@ const db = require('../config/db');
 
 const Job = {
     create: (jobData, callback) => {
-        const query = 'INSERT INTO jobs (title, description, requirements, company) VALUES (?, ?, ?, ?)';
-        db.query(query, [jobData.title, jobData.description, jobData.requirements, jobData.company], callback);
+        const { job_code, title, description, requirements, company } = jobData;
+
+        // Convert array to string
+        const requirementsString = JSON.stringify(requirements);
+
+        const query = 'INSERT INTO jobs (job_code, title, description, requirements, company) VALUES (?, ?, ?, ?, ?)';
+        db.query(query, [job_code, title, description, requirementsString, company], (err, result) => {
+            if (err) {
+                console.error('Error creating job:', err);
+                return callback(err);
+            }
+            callback(null, result.insertId);
+        });
+    },
+
+    findOne: (jobCode, callback) => {
+        const query = 'SELECT * FROM jobs WHERE job_code = ?';
+        db.query(query, [jobCode], (err, result) => {
+            if (err) {
+                console.error('Error finding job:', err);
+                return callback(err);
+            }
+            if (result.length === 0) {
+                return callback(null, null); // Job not found
+            }
+            callback(null, result[0]); // Return the first job found
+        });
     }
 };
 
